a6f8a89ffe6528aeaefc5eb753e148e2dfb671ac
masterthuku <peterthuku706@gmail.com>
2026-05-19 23:18:48 +0300

    landing page

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <body className="min-h-full flex flex-col">
           <Header />
           <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
           <footer className="bg-blue-50 py-12 ">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Copyright &copy; 2024 Vehiql. All rights reserved.</p>
